d87130a01bfdbc2b33f2367817ccbb7f747660c8
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-03-17 21:36:37 +0100

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,4 +21,15 @@ using Test
     @test p.X == X
     @test p.Y == Y
     @test p.fig == "xy"
+    T = 0:0.1:2pi
+    Y1 = sin.(T)
+    Y2 = cos.(T)
+    p = plotx(T, Y1, Y2; ylabels=["Y1", "Y2"], fig="dual")
+    display(p)      
+    plt.show(block=false)
+    sleep(1)
+    @test p.X == T
+    @test p.Y == (Y1, Y2)
+    @test p.xlabel == "time [s]"
+    @test p.ylabels == ["Y1", "Y2"]
 end
