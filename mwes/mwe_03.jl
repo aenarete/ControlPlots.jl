b08c46cecb8868057f9c5c57bf2db12267efbc25
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-05-03 14:20:21 +0200

    add mwe_03.jl

---
+++ mwes/mwe_03.jl
@@ -0,0 +1,9 @@
+using ControlPlots
+x = 1:10
+z = 2*x
+
+x_max = maximum(x)
+z_max = maximum(z)
+
+xlabel = "x [m]"
+plot(x, z; xlabel, ylabel="z [m]", xlims = (x_max-15.0, x_max+5), ylims = (z_max-15.0, z_max+5))
