9fafd926f719173ffc4b66ca3fa7f847744eb705
Uwe Fechner <fechner@aenarete.eu>
2024-08-04 15:36:16 +0200

    add example dual_y-axis.jl

---
+++ examples/dual_y-axis.jl
@@ -0,0 +1,7 @@
+using ControlPlots
+
+T = 0:0.05:2pi+0.1
+POS_Z = sin.(T)
+VEL_Z = 5*cos.(T)
+plot(T, POS_Z, VEL_Z; 
+     xlabel="time [s]", ylabels=["pos_z [m]", "vel_z [m/s]"], labels=["pos_z [m]", "vel_z [m/s]"])
