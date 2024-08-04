df30f22736f4765aceefc592d0e13d58fc629c3f
Uwe Fechner <fechner@aenarete.eu>
2024-08-04 16:22:44 +0200

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -32,4 +32,18 @@ using Test
     @test p.Y == (Y1, Y2)
     @test p.xlabel == "time [s]"
     @test p.ylabels == ["Y1", "Y2"]
+    T = 0:0.05:2pi+0.1
+    POS_Z = sin.(T)
+    VEL_Z = 5*cos.(T)
+    p = plot(T, POS_Z, VEL_Z; 
+             xlabel="time [s]", ylabels=["pos_z [m]", "vel_z [m/s]"], 
+             labels=["pos_z", "vel_z"], fig="dual_y-axis")
+    display(p)      
+    plt.show(block=false)
+    sleep(1)        
+    @test p.X == T
+    @test p.Y == [POS_Z, VEL_Z]
+    @test p.xlabel == "time [s]"
+    @test p.ylabels == ["pos_z [m]", "vel_z [m/s]"]
+    @test p.labels == ["pos_z", "vel_z"]
 end
