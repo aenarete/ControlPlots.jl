5104ff20a6ccee6b7f4b73183c159de86918db0c
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-08-05 15:11:12 +0200

    add mwe_04.jl

--- mwes/mwe_04.jl
+++ mwes/mwe_04.jl
@@ -1,2 +1,6 @@
+# for reprducing bug in ControlPlots
+# run this once, close the plot window, then run it again
+# --> crash
+
 using ControlPlots
-plot2d([[1,0,0], [2,0,0]], 10)
+plt=plot2d([[1,0,0], [2,0,0]], 10; segments=1)
