76c7289828f5a1ffa2d8f056b8e43dcd91ba67b6
Uwe Fechner <fechner@aenarete.eu>
2024-12-31 17:46:34 +0100

    improve docstring

--- src/plot.jl
+++ src/plot.jl
@@ -10,7 +10,15 @@ Plot multiple curves given by `Ys` against a common x-axis `X`.
 # Optional Arguments
 - `xlabel`: The label for the x-axis. Default is an empty string.
 - `ylabel`: The label for the y-axis. Default is an empty string.
-
+- `labels`: The labels for each curve. Default is `nothing`.
+- `xlims`: The limits for the x-axis. Default is `nothing`.
+- `ylims`: The limits for the y-axis. Default is `nothing`.
+- `ann`: An annotation to be placed on the plot. Default is `nothing`.
+- `scatter`: A boolean indicating whether to plot the points as a scatter plot. Default is `false`.
+- `fig`: The name of the figure. Default is an empty string.
+- `ysize`: The font size for the y-axis label. Default is 14.
+- `disp`: A boolean indicating whether to display the plot. If false, only create a structure to be displayed later. 
+          Default is `false`.
 """
 function plot(X, Ys::AbstractVector{<:Union{AbstractVector, Tuple}}; xlabel="", ylabel="",
     labels=nothing, xlims=nothing, ylims=nothing, ann=nothing, scatter=false, fig="", ysize=14, disp=false)
