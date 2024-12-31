3894df67f80df8dd4756ea4d0e5045e6c7a70921
Uwe Fechner <fechner@aenarete.eu>
2024-12-31 17:07:19 +0100

    improve example

--- examples/bode-plot.jl
+++ examples/bode-plot.jl
@@ -3,10 +3,4 @@ using ControlPlots
 
 P = tf([1.], [1., 1])
 
-function frequency_response(sys; from=-1, to=2)
-    w = exp10.(LinRange(from, to, 1000));
-    mag, phase, w1 = bode(sys, w)
-    w, mag[:], phase[:]
-end
-
-bode_plot(P)
+bode_plot(P; from=-2, to=2, title="Low pass filter")
