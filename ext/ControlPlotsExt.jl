7cb09728a7c3c852816c0f830ba6173acb283ef0
Uwe Fechner <fechner@aenarete.eu>
2024-12-31 17:07:10 +0100

    cleanup

--- ext/ControlPlotsExt.jl
+++ ext/ControlPlotsExt.jl
@@ -30,8 +30,8 @@ end
 todb(mag) = 20 * log10(mag)
 
 """
-    bode_plot(sys::Union{StateSpace, TransferFunction}; title="", from=-3, to=1, fig=true, 
-                                                                  db=true, Γ0=nothing)
+    bode_plot(sys::Union{StateSpace, TransferFunction}; title="", from=-3, to=1, fig=true, db=true, hz=true, Γ0=nothing,
+                                                        bw=false, linestyle="solid", title_=true, fontsize=18)
 
 Create a bode plot of a linear system. Parameters:
 - title (String)
@@ -39,18 +39,17 @@ Create a bode plot of a linear system. Parameters:
 - to    (max frequency in rad/s, default  1, means 1e1)
 - fig   (default true, create a new figure)
 - db    (default true, use dezibel as unit for the magnitude)
-- Γ0    (default nothing, if a value is passed it is attached as label to the plot)
+- hz    (default true, use Hz as unit for the frequency)
+- bw    (default false, if true, the bode plot is in black and white)
+- linestyle (default "solid", line style for the bode plot)
+- title_ (default true, show title)
+- fontsize (default 18, font size for the labels)
 
-Returns:
-`max_mag_db, omega_max` (max gain and frequency of max gain in rad/s)
+Returns: nothing
 """
 function bode_plot(sys::Union{StateSpace, TransferFunction}; title="", from=-1, to=1, fig=true, 
-                   db=true, hz=true, Γ0=nothing, bw=false, linestyle="solid", title_=true, fontsize=18, w_ex=0.0)
-    if isnothing(Γ0)
-        lbl=""
-    else
-        lbl = @sprintf "Γ: %.2f" Γ0
-    end
+                   db=true, hz=true, bw=false, linestyle="solid", title_=true, fontsize=18)
+    lbl=""
     if fig; plt.figure(title, figsize=(8, 6)); end
     ax1 = plt.subplot(211) 
     w, mag, phase = frequency_response(sys; from, to)
@@ -80,21 +79,11 @@ function bode_plot(sys::Union{StateSpace, TransferFunction}; title="", from=-1, 
     if title_
         plt.title(title, fontsize = fontsize)
     end
-    if ! isnothing(Γ0)
-        plt.legend()
-    end
     ax2 = plt.subplot(212, sharex=ax1) 
     if bw
         ax2.plot(w, phase, color="black", linestyle=linestyle)
     else
         ax2.plot(w, phase)
-    end
-
-    if w_ex != 0.0 && (! isnothing(Γ0) && Γ0 < 1)
-        corr = +8 # adds a little correction to put annotation right
-        ind = findfirst(>=(w_ex), w)
-        plt.plot(w_ex, phase[ind], linewidth=1, marker ="+", color="black", markersize=30)
-        plt.annotate(L"~ω_{ex}",  xy=(w_ex, phase[ind]+corr), fontsize = fontsize)
     end
 
     ax2.set_xscale("log")
@@ -112,9 +101,7 @@ function bode_plot(sys::Union{StateSpace, TransferFunction}; title="", from=-1, 
         plt.subplots_adjust(top=0.94)
     end
     # plt.plshow(block=false)
-    max_mag_db, index = findmax(todb.(mag))
-    omega_max = w[index]
-    max_mag_db, omega_max
+    nothing
 end
 
 end
