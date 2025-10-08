d970c2f1e6aa435d6d9c873c9e12f97781f4b07d
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-10-08 13:59:56 +0200

    Add fig parameter to plot2d.jl

--- examples/plot2d.jl
+++ examples/plot2d.jl
@@ -5,7 +5,7 @@ x0 = 2.0
 z0 = 0.0
 for t in 0:0.1:5
     global x0, z0
-    plot2d([[1,0,0], [x0,0,z0]], t; segments=1)
+    plot2d([[1,0,0], [x0,0,z0]], t; segments=1, fig="2d plot")
     x0 += 0.1; z0 += 0.1
     sleep(0.1)
 end
