1779044d3a6c391b46b717c278cfa678444fb9ee
Uwe Fechner <fechner@aenarete.eu>
2026-03-09 10:38:14 +0100

    Cleanup

--- examples/dual_one_axis_error_bars.jl
+++ examples/dual_one_axis_error_bars.jl
@@ -1,9 +1,5 @@
 using ControlPlots
 
-function plot1(X, Ys::AbstractVector{<:Union{AbstractVector, Tuple}}; xlabel="", ylabel="",
-    labels=nothing, xlims=nothing, ylims=nothing, ann=nothing, scatter=false, fig="", ysize=14, disp=false)
-end
-
 T = 0:0.2:2pi+0.1
 POS_Z = sin.(T)
 VEL_Z = 5*cos.(T)
